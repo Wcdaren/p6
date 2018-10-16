042dae1e3318505b2c13896c32368a55d86368c9
wcdaren <wcdaren@gmail.com>
2018-10-16 19:58:45 +0800

    将虚拟dom转换成真实dom渲染到页面上

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { createElement } from './element'
+import { createElement, render, renderDom } from './element'
 
 let vertualDom = createElement('ul', { class: 'list' },
     [
@@ -7,6 +7,13 @@ let vertualDom = createElement('ul', { class: 'list' },
         createElement('li', { class: 'item' }, ['c']),
     ])
 
+let el = render(vertualDom);
+// 将虚拟dom转换成真实dom渲染到页面上
+renderDom(el,window.root);
 console.log('====================================');
 console.log(vertualDom);
+console.log('====================================');
+
+console.log('====================================');
+console.log(el);
 console.log('====================================');
